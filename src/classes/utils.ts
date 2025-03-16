abef9b92f4f8be8382af2a9ff62b108c04d8350c
Nimrod <nimrod@baz.co>
2025-03-16 12:48:05 +0200

    Update utils.ts lint

--- src/classes/utils.ts
+++ src/classes/utils.ts
@@ -11,15 +11,15 @@ export async function executeQueryWithTransaction(
   query: string
 ): Promise<QueryResult> {
   const client = await pool.connect()
-  
+
   try {
-    await client.query('BEGIN')
+    await client.query("BEGIN")
     const result = await client.query(query)
-    await client.query('COMMIT')
+    await client.query("COMMIT")
     return result
   } catch (error) {
     try {
-      await client.query('ROLLBACK')
+      await client.query("ROLLBACK")
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (rollbackError) {
       // Ignore rollback errors
